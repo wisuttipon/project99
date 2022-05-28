07128420516676f260194c12926a35a131d72c1c
dew21054 <dew21054@gmail.com>
2022-05-28 17:45:48 +0700

    28/5/2565

--- component/ConversationScreen/ConnversationGroup.js
+++ component/ConversationScreen/ConnversationGroup.js
@@ -2,7 +2,16 @@ import React from "react";
 import { TouchableOpacity, Text, View, Image, ScrollView } from "react-native";
 import styles from "../styes";
 import { createStackNavigator } from "@react-navigation/stack";
-import VocabChat from "../../assets/Conversation/chat.png";
+import VocabMovie from "../../assets/Conversation/clapperboard.png";
+import VocabMuseum from "../../assets/Conversation/museum.png";
+import VocabConcert from "../../assets/Conversation/Concert.png";
+import VocabBook from "../../assets/Conversation/book.png";
+import VocabSchool from "../../assets/Conversation/school.png";
+import VocabSick from "../../assets/Conversation/sick.png";
+import VocabTest from "../../assets/Conversation/test.png";
+import VocabStudy from "../../assets/Conversation/study.png";
+import VocabCall from "../../assets/Conversation/calling.png";
+import VocabReading from "../../assets/Conversation/reading.png";
 import { useFonts } from "expo-font";
 
 const Stack = createStackNavigator();
@@ -24,7 +33,7 @@ const ConversationScreen = ({ navigation }) => {
               onPress={() => navigation.navigate("conversation1")}
               style={styles.ButtonVocabulary}
             >
-              <Image style={styles.ConLogo} source={VocabChat} />
+              <Image style={styles.ConLogo} source={VocabMovie} />
               <Text style={styles.TextCon}>Movie Conversation</Text>
               
             </TouchableOpacity>
@@ -35,7 +44,7 @@ const ConversationScreen = ({ navigation }) => {
                onPress={() => navigation.navigate("conversation2")}
               style={styles.ButtonVocabulary}
             >
-              <Image style={styles.ConLogo} source={VocabChat} />
+              <Image style={styles.ConLogo} source={VocabMuseum} />
               <Text style={styles.TextCon}>Museum Conversation </Text>
               
             </TouchableOpacity>
@@ -45,7 +54,7 @@ const ConversationScreen = ({ navigation }) => {
                onPress={() => navigation.navigate("conversation3")}
               style={styles.ButtonVocabulary}
             >
-              <Image style={styles.ConLogo} source={VocabChat} />
+              <Image style={styles.ConLogo} source={VocabConcert} />
               <Text style={styles.TextCon}>Concert Conversation </Text>
               
             </TouchableOpacity>
@@ -55,7 +64,7 @@ const ConversationScreen = ({ navigation }) => {
               onPress={() => navigation.navigate("conversation4")}
               style={styles.ButtonVocabulary}
             >
-              <Image style={styles.ConLogo} source={VocabChat} />
+              <Image style={styles.ConLogo} source={VocabBook} />
               <Text style={styles.TextCon}>Book Conversation</Text>
              
             </TouchableOpacity>
@@ -65,7 +74,7 @@ const ConversationScreen = ({ navigation }) => {
               onPress={() => navigation.navigate("conversation5")}
               style={styles.ButtonVocabulary}
             >
-              <Image style={styles.ConLogo} source={VocabChat} />
+              <Image style={styles.ConLogo} source={VocabSchool} />
               <Text style={styles.TextCon}>School Conversation</Text>
               
             </TouchableOpacity>
@@ -75,7 +84,7 @@ const ConversationScreen = ({ navigation }) => {
               onPress={() => navigation.navigate("conversation6")}
               style={styles.ButtonVocabulary}
             >
-              <Image style={styles.ConLogo} source={VocabChat} />
+              <Image style={styles.ConLogo} source={VocabSick} />
               <Text style={styles.TextCon}>Sick Conversation</Text>
               
             </TouchableOpacity>
@@ -85,7 +94,7 @@ const ConversationScreen = ({ navigation }) => {
               onPress={() => navigation.navigate("conversation7")}
               style={styles.ButtonVocabulary}
             >
-              <Image style={styles.ConLogo} source={VocabChat} />
+              <Image style={styles.ConLogo} source={VocabTest} />
               <Text style={styles.TextCon}>Test Conversation</Text>
              
             </TouchableOpacity>
@@ -95,7 +104,7 @@ const ConversationScreen = ({ navigation }) => {
               onPress={() => navigation.navigate("conversation8")}
               style={styles.ButtonVocabulary}
             >
-              <Image style={styles.ConLogo} source={VocabChat} />
+              <Image style={styles.ConLogo} source={VocabStudy} />
               <Text style={styles.TextCon}>Study Conversation</Text>
               
             </TouchableOpacity>
@@ -105,7 +114,7 @@ const ConversationScreen = ({ navigation }) => {
               onPress={() => navigation.navigate("conversation9")}
               style={styles.ButtonVocabulary}
             >
-              <Image style={styles.ConLogo} source={VocabChat} />
+              <Image style={styles.ConLogo} source={VocabCall} />
               <Text style={styles.TextCon}>Telephone call Conversation</Text>
               
             </TouchableOpacity>
@@ -115,7 +124,7 @@ const ConversationScreen = ({ navigation }) => {
               onPress={() => navigation.navigate("conversation10")}
               style={styles.ButtonVocabulary}
             >
-              <Image style={styles.ConLogo} source={VocabChat} />
+              <Image style={styles.ConLogo} source={VocabReading} />
               <Text style={styles.TextCon}>Reading book Conversation</Text>
               
             </TouchableOpacity>
